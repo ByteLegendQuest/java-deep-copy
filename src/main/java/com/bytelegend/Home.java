5f1f6da493b71f3263cf2bc71b4971ef481ca2dc
zhicheng-ning <2516085027@qq.com>
2022-01-19 03:51:13 +0000

    Update at 2022-01-19T03:51:13.144422Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -11,15 +11,13 @@ public class Home {
 
     public static void main(String[] args) {
         Home home = new Home(new Cat("Tom"));
-        Home deepCopy = home.deepCopy(home);
+        Home deepCopy = deepCopy(home);
 
         System.out.println(home.cat == deepCopy.cat);
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public Home deepCopy(Home home) {
-        Home cloned = (Home) home.clone();
-        cloned.cat = (Cat) cat.clone();
-        return cloned;
+    public static Home deepCopy(Home home) {
+        return new Home(new Cat(home.cat.name));
     }
 }
