7d668cd94df78318e84af26c248fea8b1eab7c44
o0hana <2487311872@qq.com>
2022-03-02 03:06:00 +0000

    Update at 2022-03-02T03:06:00.214263Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,8 +18,8 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home copy_of_home = new Home(new Cat(""));
-        copy_of_home = home;
-        return copy_of_home;
+       private class Home;
+       this.Home=home;
+       return Home;
     }
 }
