ef31e0f845dd12c07b82461995ad692cd758a454
xxldm <zenglengceng@outlook.com>
2022-03-15 14:03:01 +0000

    Update at 2022-03-15T14:03:01.669790Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -19,5 +19,6 @@ public class Home {
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
         home.cat = new Cat(home.cat.name);
+        return home;
     }
 }
