8d8ca30f47d05ca702efd8e984c91e3e33fd90a8
lemon-altxx <2515782282@qq.com>
2022-03-17 07:10:56 +0000

    Update at 2022-03-17T07:10:56.452089Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,9 +18,9 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home home = new Home(new Cat(home.cat.name));
+        Home homeCopy = new Home(new Cat(home.cat.name));
 
-        return home;
+        return homeCopy;
 
     }
 }
