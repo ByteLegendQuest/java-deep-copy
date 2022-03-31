9ae12d85b78a65f3b1039ff2a8168ad034012cd3
SN-Beaux <Beaux.Luo@supernovacompanies.biz>
2022-03-31 07:29:41 +0000

    Update at 2022-03-31T07:29:41.726387Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,6 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        return new Home(home.cat);
+        Home home1 = new Home(new Cat(home.cat.name));
+        return home1;
     }
 }
