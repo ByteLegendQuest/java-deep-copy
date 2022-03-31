5ef9a56fc915ad066f03ff0ff0990b17f2dcc922
hupizhu <2815695640@qq.com>
2022-03-31 11:54:52 +0000

    Update at 2022-03-31T11:54:52.747170Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -17,5 +17,8 @@ public class Home {
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home deepCopy = new Home(new Cat("shabi"));
+        return deepCopy;
+    }
 }
