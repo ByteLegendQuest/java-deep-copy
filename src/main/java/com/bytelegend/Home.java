154da37f79cee502c85396779aa305385453d310
KFig21 <Kfig21@gmail.com>
2022-05-03 04:05:54 +0000

    Update at 2022-05-03T04:05:54.712772Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,6 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        return home;
+        Home copy = new Home(new Cat(home.cat.name));
+        return copy;
     }
 }
