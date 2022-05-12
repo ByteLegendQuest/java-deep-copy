eabbf1216acd5760b2404a348ac02809e81ba4c8
Yeely0162 <Yeely01621@163.com>
2022-05-12 08:46:37 +0000

    Update at 2022-05-12T08:46:37.449716Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,7 +18,8 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home h =new Home(home.cat);
+        Home h = new Home(null);
+        h.cat = home.cat;
         return h;
     }
 }
