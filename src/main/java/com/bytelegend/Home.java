dbb3e0e0f1791a0d9eb1b45e21866f1285b98262
2809769075 <2809769075@qq.com>
2022-06-30 03:05:07 +0000

    Update at 2022-06-30T03:05:07.503147Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,7 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home home2 = home;
+        Home Cat = home;
         return home2;
     }
 }
