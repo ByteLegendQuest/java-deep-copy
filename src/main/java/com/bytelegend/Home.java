4e407f050d1d4ea638f25d73f953ea9746666b54
JudgementH <446995859@qq.com>
2022-07-06 17:07:08 +0000

    Update at 2022-07-06T17:07:08.023696Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,6 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home home = new Home(new Cat(home.cat.name));
+        Home h = new Home(new Cat(home.cat.name));
+        return h;
     }
 }
