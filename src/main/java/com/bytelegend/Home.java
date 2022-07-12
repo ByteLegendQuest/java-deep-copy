3ca69c9bd1e62b1837b4bcb26f0a0e52b7c5a179
Birlinha <omenaribeiro13@gmail.com>
2022-07-12 14:04:26 +0000

    Update at 2022-07-12T14:04:26.361435Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -15,9 +15,11 @@ public class Home {
 
         System.out.println(home.cat == deepCopy.cat);
     }
+    
     // Deep copy the instance and return the deep copy of `home`
+    // Stranger Things : The Inverse House
     public static Home deepCopy(Home home) {
-        Home strangeHome = home;
+        Home strangeHome = new Home(home.cat);
         return strangeHome;
     }
 }
