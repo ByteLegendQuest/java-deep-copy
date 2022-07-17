604fe1708984f068cff00e52ebd6a3b6dd76c8c8
GOH73 <goh73@outlook.com>
2022-07-17 02:55:21 +0000

    Update at 2022-07-17T02:55:21.978860Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -17,7 +17,7 @@ public class Home implements Cloneable{
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {
+    public static Home deepCopy(Home home) throws CloneNotSupportedException{
         Home temp = (Home)home.clone();
         return temp;
     }
