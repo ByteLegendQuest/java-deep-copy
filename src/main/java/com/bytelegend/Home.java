a3e9b12a5550f35fe3ceaa0aeef9af0d6031d799
JayeHao <15096048532@163.com>
2022-08-16 09:37:37 +0000

    Update at 2022-08-16T09:37:37.330394Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -17,5 +17,8 @@ public class Home {
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home strangeHome = new Home(new Cat(home.cat.name));
+        return strangeHome;
+    }
 }
