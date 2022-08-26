9c752e0be9119ab269579db7ee887369f417537b
BirdSnail <314726028@qq.com>
2022-08-26 20:54:36 +0800

    answer

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -17,5 +17,9 @@ public class Home {
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Cat cat = home.cat;
+        Cat newCat = new Cat(cat.name);
+        return new Home(newCat);
+    }
 }
