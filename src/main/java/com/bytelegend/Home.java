9b1e258360a4d901b6ecd434d65834658333a033
slsy0 <1485807135@qq.com>
2022-08-29 12:32:13 +0000

    Update at 2022-08-29T12:32:13.715066Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,7 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home home_dcopy = new Home(home.cat);
+        Home home_dcopy = new Home(new Cat(home.cat.name));
         return home_dcopy;
     }
 }
