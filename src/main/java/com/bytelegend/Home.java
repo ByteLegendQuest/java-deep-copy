4ecc6dc511379f0c10c9b862aff5d519b8e871c6
Mooos-MoSheng <1297393910@qq.com>
2022-10-10 09:33:46 +0000

    Update at 2022-10-10T09:33:46.497629Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,7 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home res; res.cat = home.cat;
+        Home res = new Home (home.cat);
         return res;
     }
 }
