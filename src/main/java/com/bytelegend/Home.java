21e86f11737e644dbf89b8973471a4441a5aab23
tiger0961 <29515547@qq.com>
2022-10-12 03:25:10 +0000

    Update at 2022-10-12T03:25:10.264779Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,7 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home h = new Home(home.cat);
+        Home h = new Home(new Cat(home.cat.name));
         return h;
     }
 }
