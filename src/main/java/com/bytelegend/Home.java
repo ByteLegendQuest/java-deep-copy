3662f481c696fade56462a2a4c89f5f17a9ccce9
ApoptosisToxin4869 <jiqianlang19660417@163.com>
2022-12-09 10:07:11 +0000

    Update at 2022-12-09T10:07:11.720389Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -17,5 +17,7 @@ public class Home {
     }
 
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        return home;
+    }
 }
