8bb288b548bc2df94e33b2c2cbddea142c827abf
wwwww1wwwww <1302780202@qq.com>
2022-12-26 07:21:30 +0000

    Update at 2022-12-26T07:21:30.112890Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,6 +18,7 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        return new Home(home.cat);
+        home.cat = new Cat(home.cat.name);
+        return home;
     }
 }
