f4979e3f69fce560dd34e44715705d6b1cdb86a8
CITVAIM <citvaim@icloud.com>
2023-01-24 06:08:59 +0000

    Update at 2023-01-24T06:08:59.373655Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -16,6 +16,11 @@ public class Home {
         System.out.println(home.cat == deepCopy.cat);
     }
 
+
     // Deep copy the instance and return the deep copy of `home`
-    public static Home deepCopy(Home home) {}
-}
+        public static Home deepCopy(Home home) {
+            Cat catCopy = new Cat(home.cat.getName());
+            Home homeCopy = new Home(catCopy);
+            return homeCopy;
+        }
+    }
