d96983f161004d89bbf16420f19f977a894da36e
takeITeasy0625 <1009021579@qq.com>
2023-05-29 10:07:08 +0000

    Update at 2023-05-29T10:07:08.733280Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -18,8 +18,6 @@ public class Home {
 
     // Deep copy the instance and return the deep copy of `home`
     public static Home deepCopy(Home home) {
-        Home copyHome = new Home();
-        copyHome.cat = home.cat;
-        return copyHome;
+        return new Home(home.cat);
     }
 }
